078efd1b55bc0db7a321c6d63b07595c7952873b
solo-carry <maelbizot23@gmail.com>
2021-05-24 22:15:57 +0200

    ajout fonction de trie

--- resources/views/numero.blade.php
+++ resources/views/numero.blade.php
@@ -11,22 +11,72 @@
     </head>
     <body>
     <div>
-    <h1>liste des numéros dans {{$rue}}</h1>
-       <ul class="row">
-       @foreach($les_infos as $infos)
-          <li class="col-sm-3">
-            nom de rue : {{$infos->adresse_nom_voie}} <br>
-            numéro : {{$infos->adresse_numero}} <br> 
-            valeur foncière : {{$infos->valeur_fonciere}} <br>
-            date de mutation : {{$infos->date_mutation}} <br>
-            type de local : {{$infos->type_local}} <br>
-            surface reelle bati : {{$infos->surface_reelle_bati}} <br>
-            surface du terrain : {{$infos->surface_terrain}} <br>
-          </li>
-        @endforeach
-        </ul>
+      <h1>liste des numéros dans {{$rue}}</h1>
+        <ul class="row">
+        @foreach($les_infos as $infos)
+            <li class="col-sm-3">
+              nom de rue : {{$infos->adresse_nom_voie}} <br>
+              numéro : {{$infos->adresse_numero}} <br> 
+              valeur foncière : {{$infos->valeur_fonciere}} <br>
+              date de mutation : {{$infos->date_mutation}} <br>
+              type de local : {{$infos->type_local}} <br>
+              surface reelle bati : {{$infos->surface_reelle_bati}} <br>
+              surface du terrain : {{$infos->surface_terrain}} <br>
+              nature de la mutation : {{$infos->nature_mutation}}
+            </li>
+          @endforeach
+          </ul>
+      </div>
 
-</div>
+      <form action="index.php" method="GET">
+        <label for="cars">trier par ordre : </label>
+        <select id="cars" name="carlist" form="carform">
+          <option value="asc">croissant</option>
+          <option value="desc">decroissant</option>
+          <input type="submit" name="orderby"/>
+        </select>
+        
+				<table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>adresse_nom_voie</th>
+                      <th>Description</th>
+                      <th>Quantité</th>
+                      <th>Catégorie</th>
+                      <th>Date</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      <?php
+                      use Illuminate\Http\Request;
+                      use Illuminate\Support\Facades\DB;
+
+ 
+						if(isset($_GET['carlist']) && $_GET['carlist'] == "asc") 
+						{
+   							$orderby = 'ASC';
+						}
+						else 
+						{
+					   		$orderby = 'desc';
+						}
+                        $statement = DB::table('59')->select('adresse_nom_voie', 'valeur_fonciere', 'adresse_numero', 'nature_mutation', 'date_mutation', 'type_local','nombre_pieces_principales', 'surface_reelle_bati', 'surface_terrain')->where('adresse_nom_voie', $rue)->orderBy('valeur_fonciere', $orderby)->distinct()->get();
+                        foreach ($statement as $item)
+                        {
+                            echo '<tr>';
+                            echo '<td>'. '</td>';
+                            echo '<td>'.$item->valeur_fonciere . '</td>';
+                            echo '</td>';
+                            echo '</tr>';
+                        }
+ 
+ 
+                      ?>
+                  </tbody>
+                </table>
+ 
+				</form>
     </body>
     
 </html>
