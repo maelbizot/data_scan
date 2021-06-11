0f71cff8987acd7631af0f29aca6137d56685440
solo-carry <maelbizot23@gmail.com>
2021-06-11 16:41:22 +0200

    prix m², prix vente moyen

--- resources/views/numero.blade.php
+++ resources/views/numero.blade.php
@@ -11,7 +11,7 @@
     </head>
     <body>
     <div>
-      <h1>liste des numéros dans {{$rue}}</h1>
+      <h1>liste des numéros dans {{$rue}} à {{$roh}}</h1>
         <ul class="row">
         @foreach($les_infos as $infos)
             <li class="col-sm-3">
@@ -28,60 +28,74 @@
           </ul>
       </div>
       <div class="container">
-        <h2>Prix moyen du m² {{$rue}}</h2>
-        <script>
-          
-        </script>
+        <h2>Prix moyen du m² à {{$rue}}</h2>
+        <?php 
+        $prix = 0;
+        $surface = 0;
+          $statement = DB::table('59')->select('adresse_nom_voie', 'valeur_fonciere', 'adresse_numero', 'nature_mutation', 'date_mutation', 'type_local','nombre_pieces_principales', 'surface_reelle_bati', 'surface_terrain', 'nom_commune')->where('adresse_nom_voie', $rue)->where('nom_commune', $roh)->orderBy('valeur_fonciere', 'desc')->distinct()->get();                        
+          foreach ($statement as $item)
+          {
+            // echo intval("1")+ intval("1");
+            // echo (int)$item->surface_reelle_bati. ' ';
+            // echo intval($item->surface_reelle_bati).' ';
+            //$surface += $item->surface_reelle_bati;
+            if (intval($item->surface_reelle_bati) != 0){
+              $prix += $item->valeur_fonciere;
+              $surface += intval($item->surface_reelle_bati);
+            }
+            
+          }
+          $prix = $prix/$surface;
+          $prix = number_format( $prix , 2, ',', ' ');
+          echo $prix .'€';
+        ?>
       </div>
-      
+      <div class="container">
+        <h2>Prix moyen de vente à {{$rue}}</h2>
+        <?php 
+        $prix = 0;
+        $incr = 0;
+          $statement = DB::table('59')->select('adresse_nom_voie', 'valeur_fonciere', 'adresse_numero', 'nature_mutation', 'date_mutation', 'type_local','nombre_pieces_principales', 'surface_reelle_bati', 'surface_terrain', 'nom_commune')->where('adresse_nom_voie', $rue)->where('nom_commune', $roh)->orderBy('valeur_fonciere', 'desc')->distinct()->get();                        
+          foreach ($statement as $item)
+          {
+              $prix += $item->valeur_fonciere;
+              $incr += 1;
+          }
+          $prix = $prix/$incr;
+          $prix = number_format( $prix , 3, ',', ' ');
+          echo $prix .'€';
+        ?>
+      </div>
       <div class="container">
         <h2>Liste des rues à proximité de {{$rue}}</h2>
       </div>
-      
-      <form action="index.php" method="GET">
-        <label for="cars">trier par ordre : </label>
-        <select id="cars" name="carlist" form="carform">
-          <option value="asc">croissant</option>
-          <option value="desc">decroissant</option>
-          <input type="submit" name="orderby"/>
-        </select>
+
         
-				<table class="table table-striped table-bordered">
-                  <thead>
-                    <tr>
-                      <th>adresse_nom_voie</th>
-                      <th>Description</th>
-                      <th>Quantité</th>
-                      <th>Catégorie</th>
-                      <th>Date</th>
-                      <th>Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
+            <table id="dtBasicExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+      <thead>
+        <tr>
+          <th class="th-sm">numéro
+          </th>
+          <th class="th-sm">Ville
+          </th>
+          <th class="th-sm">Prix
+          </th>
+
+        </tr>
+      </thead>
+      <tbody>
                       <?php
                       use Illuminate\Http\Request;
                       use Illuminate\Support\Facades\DB;
-
- 
-						if(isset($_GET['carlist']) && $_GET['carlist'] == "asc") 
-						{
-   							$orderby = 'ASC';
-						}
-						else 
-						{
-					   		$orderby = 'desc';
-						}
-                        $statement = DB::table('59')->select('adresse_nom_voie', 'valeur_fonciere', 'adresse_numero', 'nature_mutation', 'date_mutation', 'type_local','nombre_pieces_principales', 'surface_reelle_bati', 'surface_terrain')->where('adresse_nom_voie', $rue)->orderBy('valeur_fonciere', $orderby)->distinct()->get();
+                        $statement = DB::table('59')->select('adresse_nom_voie', 'valeur_fonciere', 'adresse_numero', 'nature_mutation', 'date_mutation', 'type_local','nombre_pieces_principales', 'surface_reelle_bati', 'surface_terrain', 'nom_commune')->where('adresse_nom_voie', $rue)->where('nom_commune', $roh)->orderBy('valeur_fonciere', 'desc')->distinct()->get();                        
                         foreach ($statement as $item)
                         {
-                            echo '<tr>';
-                            echo '<td>'. '</td>';
+                            echo '<td>'. $item->adresse_numero .'</td>' ;
+                            echo '<td>'. $item->nom_commune .'</td>';
                             echo '<td>'.$item->valeur_fonciere . '</td>';
                             echo '</td>';
                             echo '</tr>';
                         }
- 
- 
                       ?>
                   </tbody>
                 </table>
@@ -95,4 +109,27 @@
 li{
   margin : 25px;
 }
+
+table.dataTable thead .sorting:after,
+table.dataTable thead .sorting:before,
+table.dataTable thead .sorting_asc:after,
+table.dataTable thead .sorting_asc:before,
+table.dataTable thead .sorting_asc_disabled:after,
+table.dataTable thead .sorting_asc_disabled:before,
+table.dataTable thead .sorting_desc:after,
+table.dataTable thead .sorting_desc:before,
+table.dataTable thead .sorting_desc_disabled:after,
+table.dataTable thead .sorting_desc_disabled:before {
+bottom: .5em;
+}
 </style>
+
+<script>
+// Basic example
+$(document).ready(function () {
+$('#dtBasicExample').DataTable({
+"ordering": false // false to disable sorting (or any other option)
+});
+$('.dataTables_length').addClass('bs-select');
+});
+</script>
